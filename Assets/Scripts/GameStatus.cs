b642ab1758a6ada3695be0145c03e7b0886fd662
istoicheva <ina.stoicheva@gmail.com>
2019-05-06 10:50:21 +0300

    Updated sound files; added score singalton

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
 
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] TextMeshProUGUI scoreText;
+
     [SerializeField] int currentScore = 0;
+
+    // The very first method that is initialized
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,5 +39,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore(int pointsPerBlockDestoryed)
     {
         currentScore += pointsPerBlockDestoryed;
+        scoreText.text = currentScore.ToString();
     }
 }
